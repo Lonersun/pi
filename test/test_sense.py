b53190c0f7d6431b9352a96a938038124eb9a081
GuoYang0803 <yang.guo.sun@gmail.com>
2017-06-08 23:10:44 +0800

    add sense test

--- test/test_sense.py
+++ test/test_sense.py
@@ -7,10 +7,13 @@ sys.setdefaultencoding("utf-8")
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from pi_modules.sense import sense
+from pi_modules.beep import beep
 
 sense_server = sense.Sense(12)
 result = sense_server.detct()
 if result is True:
+    beep_server = beep.Beep(21)
     print "附近有人"
+    beep_server.interval_beep(5)
 else:
     print "附近无人"
